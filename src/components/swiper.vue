c0f11235c2434c0d8e7dc190d17f29b15fb21359
islamnashaat03 <islamnashaat03@gmail.com>
2022-02-07 23:48:55 +0200

    add swipee"

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -39,6 +39,10 @@
         </swiper-slide>
 
         <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>
     <div class="swiper-button-next" slot="button-next"></div>
       </swiper>
